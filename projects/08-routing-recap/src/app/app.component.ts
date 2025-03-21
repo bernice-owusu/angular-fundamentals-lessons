cc46c6726a8cf23f17877cf35d66952e4e10b518
bernice <bernyx.owusu@gmail.com>
2025-03-21 14:57:30 +0000

    router

--- projects/08-routing-recap/src/app/app.component.ts
+++ projects/08-routing-recap/src/app/app.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: ` <h1>Welcome to {{ title }}!</h1> `,
+  imports: [RouterOutlet, RouterLink],
+  template: ` 
+  <h1>Welcome to {{ title }}!</h1> 
+  @for( productTitle of productTitles; track productTitle;)  {
+   <p>
+   <a [routerLink]="['details', $index]">{{productTitle}}</a>
+   </p>
+ 
+  }
+  <router-outlet/>
+  `,
 })
 export class AppComponent {
   title = '08-routing-recap';
