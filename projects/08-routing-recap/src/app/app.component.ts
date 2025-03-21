f1dad580ec42e65e079a012681f10feacea12778
bernice <bernyx.owusu@gmail.com>
2025-03-21 15:14:52 +0000

    router-update

--- projects/08-routing-recap/src/app/app.component.ts
+++ projects/08-routing-recap/src/app/app.component.ts
@@ -7,9 +7,9 @@ import { RouterOutlet, RouterLink } from '@angular/router';
   imports: [RouterOutlet, RouterLink],
   template: ` 
   <h1>Welcome to {{ title }}!</h1> 
-  @for( productTitle of productTitles; track productTitle;)  {
+  @for( title of productTitles; track title;)  {
    <p>
-   <a [routerLink]="['details', $index]">{{productTitle}}</a>
+      <a [routerLink]="['details', $index]" >{{title}}</a>
    </p>
  
   }
--- projects/08-routing-recap/src/app/app.routes.ts
+++ projects/08-routing-recap/src/app/app.routes.ts
@@ -10,5 +10,5 @@ export const routes: Routes = [
     {
         path: 'details/:id',
         component: DetailsComponent
-    }
+    },
 ];
